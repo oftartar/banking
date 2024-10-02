fa90486d4d9e6755bb2309bd0ba4c047ee86fe75
Ömer Faruk TARTAR <oftartar@gmail.com>
2024-10-02 11:56:11 +0300

    implement category component

---
+++ components/ui/progress.tsx
@@ -0,0 +1,39 @@
+"use client";
+
+import * as React from "react";
+import * as ProgressPrimitive from "@radix-ui/react-progress";
+
+import { cn } from "@/lib/utils";
+import { ProgressProps } from "@radix-ui/react-progress";
+
+interface ProgressIndicatorProps extends ProgressProps {
+  value?: number | null | undefined;
+  max?: number;
+  getValueLabel?(value: number, max: number): string;
+  indicatorClassName: string | undefined;
+}
+
+const Progress = React.forwardRef<
+  React.ElementRef<typeof ProgressPrimitive.Root>,
+  ProgressIndicatorProps
+>(({ indicatorClassName, className, value, ...props }, ref) => (
+  <ProgressPrimitive.Root
+    ref={ref}
+    className={cn(
+      "relative h-4 w-full overflow-hidden rounded-full bg-secondary",
+      className
+    )}
+    {...props}
+  >
+    <ProgressPrimitive.Indicator
+      className={cn(
+        "h-full w-full flex-1 bg-primary transition-all",
+        indicatorClassName
+      )}
+      style={{ transform: `translateX(-${100 - (value || 0)}%)` }}
+    />
+  </ProgressPrimitive.Root>
+));
+Progress.displayName = ProgressPrimitive.Root.displayName;
+
+export { Progress };
